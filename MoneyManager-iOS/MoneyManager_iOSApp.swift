f6fd7f665b42160aac72f0601e9fc663c3365f69
MahiAlJawad <br.mahialjawad@gmail.com>
2024-10-01 21:01:50 +0600

    Initial Commit

---
+++ MoneyManager-iOS/MoneyManager_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManager_iOSApp.swift
+//  MoneyManager-iOS
+//
+//  Created by Mahi Al Jawad on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManager_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
