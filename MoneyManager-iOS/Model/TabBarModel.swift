1bf6e056631d7f46041453d14d6d1e330090b53f
MahiAlJawad <br.mahialjawad@gmail.com>
2024-10-04 22:35:41 +0600

    Added tab bar structure

---
+++ MoneyManager-iOS/Model/TabBarModel.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarModel.swift
+//  MoneyManager-iOS
+//
+//  Created by Mahi Al Jawad on 4/10/24.
+//
+
+import Foundation
+
+struct TabBarModel {
+    enum Item: CaseIterable {
+        case dashboard
+        case accounts
+        case statistics
+        case planning
+        case more
+        
+        var title: String {
+            switch self {
+            case .dashboard:    return "Dashboard"
+            case .accounts:     return "Accounts"
+            case .statistics:   return "Statistics"
+            case .planning:     return "Planning"
+            case .more:         return "More"
+            }
+        }
+        
+        var icon: String {
+            switch self {
+            case .dashboard:  return "dollarsign.bank.building.fill"
+            case .accounts:   return "note.text"
+            case .statistics: return "chart.bar.xaxis"
+            case .planning:   return "calendar.badge.clock"
+            case .more:       return "ellipsis.circle.fill"
+            }
+        }
+    }
+}
---
+++ MoneyManager-iOS/View/TabBar/TabBarView.swift
@@ -0,0 +1,47 @@
+//
+//  TabBarView.swift
+//  MoneyManager-iOS
+//
+//  Created by Mahi Al Jawad on 1/10/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    typealias Tab = TabBarModel.Item
+    @State var selectedTab: Tab = .dashboard
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Coming Soon")
+                .tabItem {
+                    Label(Tab.dashboard.title, systemImage: Tab.dashboard.icon)
+                }
+                .tag(Tab.dashboard)
+            
+            Text("Coming Soon")
+                .tabItem {
+                    Label(Tab.accounts.title, systemImage: Tab.accounts.icon)
+                }
+                .tag(Tab.accounts)
+            
+            Text("Coming Soon")
+                .tabItem {
+                    Label(Tab.statistics.title, systemImage: Tab.statistics.icon)
+                }
+                .tag(Tab.statistics)
+            
+            Text("Coming Soon")
+                .tabItem {
+                    Label(Tab.planning.title, systemImage: Tab.planning.icon)
+                }
+                .tag(Tab.planning)
+            
+            Text("Coming Soon")
+                .tabItem {
+                    Label(Tab.more.title, systemImage: Tab.more.icon)
+                }
+                .tag(Tab.more)
+        }
+    }
+}
