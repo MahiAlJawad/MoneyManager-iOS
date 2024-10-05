20f9715cdc0a0933ef90130f817b50451f69b29d
MahiAlJawad <br.mahialjawad@gmail.com>
2024-10-05 19:07:25 +0600

    Made prject min version iOS 17 and updated Accounts view

--- MoneyManager-iOS/View/Accounts/AccountsView.swift
+++ MoneyManager-iOS/View/Accounts/AccountsView.swift
@@ -12,23 +12,41 @@ struct AccountsView: View {
     @Environment(\.modelContext) var modelContext
     @Query var accounts: [Account]
     
+    var debitAccounts: [Account] {
+        accounts.filter({ $0.accountType == Account.AccountType.debit.description })
+    }
+    
+    var creditAccounts: [Account] {
+        accounts.filter({ $0.accountType == Account.AccountType.credit.description })
+    }
+    
+    func accountView(for account: Account) -> some View {
+        VStack(alignment: .leading) {
+            Text(account.accountName)
+                .font(.headline)
+            Text(account.accountBalance, format: .currency(code: "BDT"))
+            Text(account.accountType)
+                .font(.caption)
+        }
+    }
+    
     var body: some View {
         List {
-            // TBD: Will be separated according to the types
-            ForEach(accounts) { account in
-                VStack(alignment: .leading) {
-                    Text(account.accountName)
-                        .font(.headline)
-                    Text(account.accountBalance, format: .currency(code: "BDT"))
-                    Text(account.accountType)
-                        .font(.caption)
+            Section("Debit Accounts") {
+                ForEach(debitAccounts) { account in
+                    accountView(for: account)
+                }
+            }
+            Section("Credit Accounts") {
+                ForEach(creditAccounts) { account in
+                    accountView(for: account)
                 }
             }
         }
         .navigationTitle("Accounts")
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
-                // TBD
+                // TODO: Add operation will be handled in a seperate view
                 Button("Add", systemImage: "plus.circle.fill") {
                     let account = Account(name: "Demo Account \(Int.random(in: 1...100))", balance: Double.random(in: 1...1000), type: Bool.random() ? .debit : .credit)
                     
