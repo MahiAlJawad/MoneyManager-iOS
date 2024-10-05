2595813979d7562ca2c5e36a6f1e2bb689d6ebdf
MahiAlJawad <br.mahialjawad@gmail.com>
2024-10-05 13:14:06 +0600

    Added Account model

---
+++ MoneyManager-iOS/Model/Account.swift
@@ -0,0 +1,38 @@
+//
+//  Account.swift
+//  MoneyManager-iOS
+//
+//  Created by Mahi Al Jawad on 5/10/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Account {
+    enum AccountType {
+        case debit
+        case credit
+        
+        var description: String {
+            switch self {
+            case .debit: return "Debit"
+            case .credit: return "Credit"
+            }
+        }
+    }
+    
+    private var id: String
+    private var name: String
+    private var balance: Double
+    private var type: String
+    
+    init(name: String, balance: Double, type: AccountType) {
+        id = UUID().uuidString
+        self.name = name
+        self.balance = balance
+        self.type = type.description
+    }
+}
+
+
