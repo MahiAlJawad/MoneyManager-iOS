2b3c7662e5983b12a9995fa2a5be95d7078bdc24
MahiAlJawad <br.mahialjawad@gmail.com>
2024-10-09 23:13:37 +0600

    Resolved reviews

--- MoneyManager-iOS/Model/Transaction.swift
+++ MoneyManager-iOS/Model/Transaction.swift
@@ -22,14 +22,14 @@ class Transaction {
         
         var description: String {
             switch self {
-            case .cash: return "Cash"
-            case .creditCard: return "Credit Card"
-            case .debitCard: return "Debit Card"
-            case .banktransfer: return "Bank Transfer"
-            case .voucher: return "Voucher"
-            case .mobilePayment: return "Mobile Payment"
-            case .webPayment: return "Web Payment"
-            case .other: return "Other"
+            case .cash:             return "Cash"
+            case .creditCard:       return "Credit Card"
+            case .debitCard:        return "Debit Card"
+            case .banktransfer:     return "Bank Transfer"
+            case .voucher:          return "Voucher"
+            case .mobilePayment:    return "Mobile Payment"
+            case .webPayment:       return "Web Payment"
+            case .other:            return "Other"
             }
         }
     }
@@ -41,13 +41,14 @@ class Transaction {
         
         var description: String {
             switch self {
-            case .expense: return "Expense"
-            case .income: return "Income"
-            case .transfer: return "Transfer"
+            case .expense:          return "Expense"
+            case .income:           return "Income"
+            case .transfer:         return "Transfer"
             }
         }
     }
     
+    var id: String
     private var type: String
     private var ammount: Double
     private var category: String
@@ -62,6 +63,7 @@ class Transaction {
         date: Date,
         labels: [String]
     ) {
+        self.id = UUID().uuidString
         self.type = type.description
         self.ammount = Double(amount) ?? 0
         self.category = category.name
--- MoneyManager-iOS/View/AddTransactionView/AddTransactionView.swift
+++ MoneyManager-iOS/View/AddTransactionView/AddTransactionView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct AddTransactionView: View {
     @Environment(\.dismiss) private var dismiss
-    @State private var addTransactionInfo: AddTransactionInfo = .init()
+    @State private var addTransactionInfo = AddTransactionInfo()
     @FocusState private var focusField: FocusField?
     @State private var bgColor = Color.gray.opacity(0.2)
     
